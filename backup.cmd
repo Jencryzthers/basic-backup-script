35caf665004379a90877daf3b2bd01f90d7b3024
Jean-Christophe Proulx <j.christophe@devjc.net>
2021-04-20 21:07:11 -0400

    Updated the readme

--- backup.cmd
+++ backup.cmd
@@ -1,3 +1,11 @@
-@ECHO OFF
-python "c:/Program Files (x86)/World of Warcraft/_retail_/backup.py" -target "C:\Program Files (x86)\World of Warcraft\_retail_\_backup" -source "C:\Program Files (x86)\World of Warcraft\_retail_\interface"
-python "c:/Program Files (x86)/World of Warcraft/_retail_/backup.py" -target "C:\Program Files (x86)\World of Warcraft\_retail_\_backup" -source "C:\Program Files (x86)\World of Warcraft\_retail_\wtf"
+# Basic Backup Script 
+### (That I used for World of Warcraft Addons and settings backups)
+
+This script is a python3 script that backup and compress the addons folder WTF and Interface into _backup folder and keep 5 day history to prevent any dataloss from buggy addons that could wipe all the settings.
+
+
+# How-To
+
+Put the file in your world of warcraft directory and than map the vbs to your task scheduler in Windows or in your shell:startup to have automatic backup.
+
+;)
